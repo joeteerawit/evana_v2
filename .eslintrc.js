a78d2962b0f0494c9c625823320d992442b93f5b
Joewalker <teerawit@hashpays.io>
2025-02-25 09:43:51 +0700

    upgrade package

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: ['import'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+}
